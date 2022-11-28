6899d4fcfde2096acdd7fe88c02a89d5b845a615
Yanis <56721402+Illusion-97@users.noreply.github.com>
2022-11-28 17:13:29 +0100

    Init Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SousMenuComponent } from './components/menu/sous-menu/sous-menu.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    SousMenuComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
