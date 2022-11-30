853822980857abae7bd0b5394d0ecae69eec5638
Yanis <56721402+Illusion-97@users.noreply.github.com>
2022-11-30 17:36:51 +0100

    Demo : ngIf / ngFor / ng-container

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Post} from "../../models/post";
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  posts: Post[] = [
+    {
+      titre: "Interdum aenean",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic01.jpg",
+      imageAlt: "",
+      postLink: "#"
+    },
+    {
+      titre: "Nulla amet dolore",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "",
+      imageAlt: "",
+      postLink: "#"
+    },
+    {
+      titre: "Tempus ullamcorper",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic03.jpg",
+      imageAlt: "",
+      postLink: "#"
+    },
+    {
+      titre: "Sed etiam facilis",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic04.jpg",
+      imageAlt: "",
+      postLink: "#"
+    },
+    {
+      titre: "Feugiat lorem aenean",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "",
+      imageAlt: "",
+      postLink: "#"
+    },
+    {
+      titre: "Amet varius aliquam",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic06.jpg",
+      imageAlt: "",
+      postLink: "#"
+    },
+  ]
 
   constructor() { }
 
