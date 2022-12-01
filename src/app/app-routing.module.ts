57007079cd5ab1e000aef34053a1ca95a9f7903f
Yanis <56721402+Illusion-97@users.noreply.github.com>
2022-12-01 16:52:47 +0100

    Demo : Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {EditorComponent} from "./pages/editor/editor.component";
+import {FormulaireComponent} from "./components/formulaire/formulaire.component";
+import {FormControlComponent} from "./components/form-control/form-control.component";
+import {FormGroupComponent} from "./components/form-group/form-group.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'editor', component: EditorComponent, children: [
+      {path: '', component: FormulaireComponent},
+      {path: 'form-control', component: FormControlComponent},
+      {path: 'reactive-form', component: FormGroupComponent},
+      {path: 'reactive-form/:titre', component: FormGroupComponent}
+    ]},
+  {path: '**', component: HomeComponent} // Toujours en dernier (404)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/form-group/form-group.component.ts
+++ src/app/components/form-group/form-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
 
 
 @Component({
@@ -19,9 +20,16 @@ export class FormGroupComponent implements OnInit {
     postLink: new FormControl('', [Validators.required, this.isBlankValidator()])
   })
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Récupère la valeur en cours de la variable titre présente dans l'URL
+    const titreActuel = this.route.snapshot.paramMap.get('titre')
+    // Il est possible de récupérer des informations avec this.route.snapshot.queryParamMap
+    if(titreActuel) { // Si j'ai bien récupéré une valeur
+      // Je met a jour la valeur du formControl 'titre'
+      this.formGroup.controls['titre'].setValue(titreActuel)
+    }
   }
 
   onSubmit() {
